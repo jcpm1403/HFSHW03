973a99579ff3fe89a315d050ec66d91b2b545d12
jcpm <jcpm140382@gmail.com>
2024-07-24 21:39:36 -0400

    Changes in many Code Lines

---
+++ funciones.js
@@ -0,0 +1,18 @@
+function sumaTres(x) {          //Función: sumaTres
+    console.log (x + 3);
+}
+sumaTres (5);
+
+var sumaCuatro = (x) => {       //Función: sumaCuatro
+    return x + 4;
+  }
+
+var sumaCinco = function (x) {  //Función: sumaCinco
+    return x + 5;
+}
+
+function cuidadoConElConsoleLog(nombre) {       //Función: cuidadoConElConsoleLog
+    console.log(nombre);
+    return nombre;
+}
+cuidadoConElConsoleLog ("Juan");
